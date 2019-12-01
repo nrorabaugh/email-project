5a4e8848bbabd85041234892a93b34b17f135da3
Noah Rorabaugh <noahrorabaugh@Noahs-MBP.attlocal.net>
2019-12-01 15:10:41 -0500

    Compose email post

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Inbox from './Inbox'
 import Sent from './Sent'
+import Compose from './Compose'
 export default class Account extends Component {
     state = {
         component: 'Inbox'
@@ -16,6 +17,7 @@ export default class Account extends Component {
             <div className='page'>
                 <div className='nav'>
                     <ul>
+                        <button onClick={this.shiftComponent}><li>Compose</li></button>
                         <button onClick={this.shiftComponent}><li>Inbox</li></button>
                         <button onClick={this.shiftComponent}><li>Sent</li></button>
                         <button onClick={this.shiftComponent}><li>Contacts</li></button>
@@ -25,6 +27,7 @@ export default class Account extends Component {
                 <div className='switch'>
                     {this.state.component ==='Inbox'? <Inbox currentUser={this.props.currentUser}/> : null}
                     {this.state.component ==='Sent'? <Sent currentUser={this.props.currentUser}/> : null}
+                    {this.state.component ==='Compose'? <Compose currentUser={this.props.currentUser}/> : null}
                 </div>
             </div>
             </div>
---
+++ client/src/components/Compose.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Inbox extends Component {
+    mail = (evt) => {
+        evt.preventDefault()
+        let email = {
+            sender: this.props.currentUser.id,
+            receiver: evt.target.receiver.value,
+            title: evt.target.title.value,
+            content: evt.target.content.value
+        }
+        axios.post('/api/v1/email/', email)
+    }
+    
+    render() {
+        return (
+            <form className='emailComp' onSubmit={this.mail}>
+                <input type='text' name='receiver' placeholder='To:'/>
+                <input type='text' name='title' placeholder='Title'/>
+                <textarea className='body' type='text' name = 'content'/>
+                <input type='submit' value='Send'/>
+            </form>
+        )
+    }
+}
