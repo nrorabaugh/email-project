d41773f03e1fb9d4e59ee3b9d52954636ef342e4
Noah Rorabaugh <noahrorabaugh@Noahs-MBP.attlocal.net>
2019-12-02 21:05:50 -0500

    Styled email

--- client/src/components/Email.js
+++ client/src/components/Email.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Email extends Component {
+    state = {
+        sender: '',
+        receiver: ''
+    }
+    
     render() {
         console.log(this.props.email)
         return (
             <div className='emailText'>
                 <h1>{this.props.email.title}</h1>
-                <p className='toFrom'>From {this.props.email.sender}</p>
-                <p>{this.props.email.content}</p>
+                <div className='toFrom'>
+                <p>From {this.props.email.sender}</p>
+                <p>To {this.props.email.receiver}</p>
+                </div>
+                <p className='content'>{this.props.email.content}</p>
             </div>
         )
     }
